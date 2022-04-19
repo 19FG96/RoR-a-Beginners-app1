f16a7f98ece71bb90efaf75c2a574644b207ef92
19FG96 <fgolubicek@outlook.com>
2022-04-19 19:36:47 +0000

    fix error heroku email

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,11 +1,11 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
-  :port => '587',
+  :port => 587,
   :authentication => :plain,
-  :user_name => ENV["SENDGRID_USERNAME"],
-  :password => ENV["SENDGRID_PASSWORD"],
-  :domain => 'heroku.com',
+  :user_name => "apikey",
+  :password => "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",
+  :domain => '78d62ee457934e4eb77c5b1d6ae04715.vfs.cloud9.us-east-1.amazonaws.com',
   :enable_starttls_auto => true 
   
 }
